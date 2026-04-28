df1ac861d9b208181674e913a966a596c814cbe7
Prathamesh Dande <prathameshdande7@gmail.com>
2026-04-28 14:34:25 +0530

    Looping-statement

---
+++ Loops.java
@@ -0,0 +1,145 @@
+
+import java.util.Scanner;
+
+public class Loops {
+
+    // public static boolean isPrime(int n){
+    //         if(n <= 1){
+    //             return false;
+    //         }
+
+    //         for(int i = 2; i <= Math.sqrt(n); i++){
+    //             if( n % i == 0){
+    //                 return false;
+    //             }
+    //         }
+
+    //         return true;
+    //     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // for(int i=1; i<=10; i++){
+        //     System.out.println("Prathamesh      ");
+        // }
+
+        // for(int i=2; i<=20; i+=2){
+        //     System.out.println("Number is : " + i );
+        // }
+
+        // for(int i=1; i<=5; i++){
+        //     for(int j=1; j<=5; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=1; i<=5; i++){
+        //     for(int j=1; j<=5; j++){
+        //         System.out.print(" i= "+i+" j= "+j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=1; i<=10; i++){
+        //     if(i == 5){
+        //         // break;
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        //while loop
+
+        // int i = 1;
+        // while(i<=10){
+        //     System.out.print(i+" ");
+        //     i++;
+        // }
+
+        // int i=1; 
+        // while(i<=5){
+        //     int j=1;
+        //     while(j<=i){
+        //         System.out.print(j+" ");
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
+        // int i=1;
+
+        // do { 
+        //     System.out.println(i);
+        //     i++;
+        // } while (i<=5);
+
+        // do { 
+        //     System.out.println(i);
+        //     i++;
+        // } while (i<0);
+
+        //print counting from 1-n
+        // System.out.println("Enter the number you want to print: ");
+        // int n = sc.nextInt();
+        
+        // for(int i=1; i<=n; i++){
+        //     System.out.println(i);
+        // }
+
+        //print counting from n to 1
+
+        // print the 10 multi of n
+        // System.out.println("Enter the number you want to print of 10 multiples of number: ");
+        // int n = sc.nextInt();        
+        // for(int i = 1; i <= 10; i++){
+        //     System.out.println(i+" x "+n+" = "+i*n);
+        // }
+
+        //Print your number 100 times
+        // System.out.println("Enter your name that do you want to print 100 times: ");
+        // String n = sc.next();  
+        // for(int i = 1; i <= 100; i++){
+        //     System.out.println(i+") "+n);
+        // }
+
+        //print all prime numbers 1 to 100
+        // for(int i = 3; i <= 100; i++){
+        //     if(isPrime(i)){
+        //         System.out.println("The "+i+" is prime number");
+        //     } else{
+        //         System.out.println("The "+i+" is not prime number");
+        //     }
+        // }
+
+
+        //print the even numbers between ranges from 1 to 100
+        // System.out.println("The even numbers from 1 to 100 is : ");
+        // for(int i = 0; i <= 100; i++){
+        //     if(i % 2 == 0){
+        //         System.out.println(i);
+        //     }
+        // }
+
+        //print the sum of all number from 1 to n
+
+        // int sum = 0;
+        // System.out.println("Enter the number : ");
+        // int num = sc.nextInt();
+        // for(int i = 1; i <= num; i++){
+        //     sum += i;
+        // }
+        
+        // System.out.println("The sum is : "+sum);
+
+        //print the all integers ranges from 50 to 100 that are all divisible by 7
+
+        // System.out.println("The number which is divisible by 7 in range of 50 to 100");
+        // for(int i = 50; i <= 100; i++){
+        //     if(i % 7 == 0){
+        //         System.out.println(i);
+        //     }
+        // }
+    }
+}
